ba6ccf6394c9ed4474a636a60591032414c6e6ff
MadhurSud <madhursud@gmail.com>
2024-01-17 10:08:19 -0600

    Added css styling

--- pdareactsite/src/App.js
+++ pdareactsite/src/App.js
@@ -1,14 +1,48 @@
-import ProfileCard from './ProfileCard'
+import 'bulma/css/bulma.css';
+import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 function App()
 {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>
+                </div>
+            </section>
 
-            <ProfileCard title='Alexa' handle='@alexa99' color='red'/>
-            <ProfileCard title='Cortana' handle='@cortana32'  color='blue'/>
-            <ProfileCard title='Siri' handle='@siri01' color='yellow'/>
+            < div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCard title='Alexa' 
+                            handle='@alexa99' 
+                            color='red' 
+                            image={AlexaImage}
+                            description="Alexa was created by Amazon and helps you buy things"/>
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard title='Cortana'
+                             handle='@cortana32'  
+                             color='blue' 
+                             image={CortanaImage}
+                             description="Cortana was created by Microsoft and who knows what it does"
+                             />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard title='Siri' 
+                            handle='@siri01' 
+                            color='yellow' 
+                            image={SiriImage}
+                            description="Siri was created by Apple and is being phased out"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
